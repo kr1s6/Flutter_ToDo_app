72b0d8703fc19f4d25efa2b04f3c4e6ffcb5f559
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-07-27 18:37:50 +0200

    place for note's title

--- flutter_todo_app/lib/create_note.dart
+++ flutter_todo_app/lib/create_note.dart
@@ -5,6 +5,107 @@ import 'package:provider/provider.dart';
 class NewNotePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      // ----------------------TOP--------------------------------
+      appBar: AppBar(
+        title: SizedBox(
+          height: 35,
+          child: Tooltip(
+            message: "title",
+            child: TextField(
+              textAlignVertical: TextAlignVertical.bottom,
+              style: Theme.of(context).textTheme.titleMedium,
+              maxLength: 25,
+              maxLines: 1,
+              decoration: InputDecoration(
+                counterText: "",
+                filled: true,
+                fillColor: Color.fromARGB(255, 213, 230, 209),
+                border: OutlineInputBorder(),
+                hintText: "Title",
+                hintStyle: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      // ----------------------Center-------------------------------
+      body: _NoteBody(),
+      // ----------------------BOTTOM--------------------------------
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'save',
+        onPressed: () {
+          appState.changePage();
+        },
+        backgroundColor: const Color.fromARGB(255, 76, 196, 80),
+        shape: ShapeBorder.lerp(CircleBorder(), CircleBorder(), 0),
+        child: const Icon(Icons.save),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: _CustomBottomAppBar(height: 40),
+    );
+  }
+}
+
+class _NoteBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var notesList = appState.notesList;
+
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        if (notesList.isNotEmpty)
+          for (var note in notesList) note
+        else
+          Container(
+            alignment:
+                AlignmentGeometry.lerp(Alignment.center, Alignment.center, 0),
+            child: Text(
+              "Add your first note",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          )
+      ],
+    );
+  }
+}
+
+class _CustomBottomAppBar extends StatelessWidget {
+  const _CustomBottomAppBar({this.height = 40});
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return BottomAppBar(
+      notchMargin: 4,
+      padding: EdgeInsets.all(5),
+      shape: const CircularNotchedRectangle(),
+      clipBehavior: Clip.antiAlias,
+      height: height,
+      color: Color.fromARGB(255, 76, 175, 102),
+      // -------------------------------------------------
+      child: IconTheme(
+        data: IconThemeData(color: Colors.black),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              padding: EdgeInsets.all(0),
+              tooltip: 'back',
+              icon: const Icon(Icons.chevron_left_rounded),
+              onPressed: () {
+                appState.changePage();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- flutter_todo_app/lib/notes_page.dart
+++ flutter_todo_app/lib/notes_page.dart
@@ -6,51 +6,56 @@ class NotesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var notesList = appState.notesList;
-    final theme = Theme.of(context);
-    final style = theme.textTheme.titleLarge!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
+    // final theme = Theme.of(context);
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       // ----------------------TOP--------------------------------
       appBar: AppBar(
-        toolbarHeight: 40,
-        backgroundColor: Colors.green[700],
         title: const Text(
           'ToDo',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
       // ----------------------Center-------------------------------
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        children: [
-          if (notesList.isNotEmpty)
-            for (var note in notesList)
-              Container(
-                child: note,
-              )
-          else
-            Container(
-              alignment:
-                  AlignmentGeometry.lerp(Alignment.center, Alignment.center, 0),
-              child: Text("Empty"),
-            )
-        ],
-      ),
+      body: _BodyNotesList(),
       // ----------------------BOTTOM--------------------------------
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           appState.changePage();
         },
-        backgroundColor: Colors.green,
+        backgroundColor: const Color.fromARGB(255, 76, 196, 80),
         shape: ShapeBorder.lerp(CircleBorder(), CircleBorder(), 0),
+        tooltip: 'new note',
         child: const Icon(Icons.create),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: _CustomBottomAppBar(height: 40),
+    );
+  }
+}
+
+class _BodyNotesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var notesList = appState.notesList;
+
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        if (notesList.isNotEmpty)
+          for (var note in notesList) note
+        else
+          Container(
+            alignment:
+                AlignmentGeometry.lerp(Alignment.center, Alignment.center, 0),
+            child: Text(
+              "Add your first note",
+              style: TextStyle(fontSize: 15),
+            ),
+          )
+      ],
     );
   }
 }
@@ -62,7 +67,7 @@ class _CustomBottomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      notchMargin: 3,
+      notchMargin: 4,
       padding: EdgeInsets.all(5),
       shape: const CircularNotchedRectangle(),
       clipBehavior: Clip.antiAlias,
